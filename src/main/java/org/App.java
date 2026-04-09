26be841402e97504f11731fab313af3834d3a84d
wooki0123 <wookimini22@gmail.com>
2026-04-09 11:08:48 +0900

    refacotr: 등록, 목록 기능 함수 분리

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,4 +1,59 @@
 package org;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
+    Scanner scanner = new Scanner(System.in);
+    int lastId = 0;
+    List<WiseSaying> wiseSayingList = new ArrayList<>();
+
+    void run() {
+
+        System.out.println("==명언 앱==");
+        while (true) {
+            System.out.print("명령)");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                exit();
+                break;
+            } else if (cmd.equals(("등록"))) {
+                actionWrite();
+            } else if (cmd.equals("목록")) {
+                actionList();
+            }
+        }
+    }
+    void exit() {
+        System.out.println("프로그램 종료");
+    }
+    void actionWrite() {
+        System.out.print("명언: ");
+        String wiseSayingContent = scanner.nextLine().trim();
+
+        System.out.print("작가: ");
+        String wiseSayingAuthor = scanner.nextLine().trim();
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", lastId + 1);
+
+        WiseSaying wiseSaying = new WiseSaying();
+        wiseSaying.id = lastId;
+        wiseSaying.content = wiseSayingContent;
+        wiseSaying.author = wiseSayingAuthor;
+
+        wiseSayingList.add(wiseSaying);
+
+        lastId++;
+    }
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-------------------------");
+
+        for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
+            WiseSaying wiseSaying = wiseSayingList.get(i);
+            System.out.printf("%d / %s / %s\n".formatted(wiseSaying.id+1, wiseSaying.content, wiseSaying.author));
+        }
+    }
 }
--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,36 +1,8 @@
 package org;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("==명언 앱==");
-        Scanner scanner = new Scanner(System.in);
-        int lastId = 1;
-
-        while (true) {
-            System.out.println("명령)");
-            String cmd = scanner.nextLine().trim();
-            
-            if(cmd.equals("종료")) {
-                System.out.println("프로그램 종료");
-                break;
-            } else if (cmd.equals(("등록"))) {
-                System.out.println("명언: ");
-                String wiseSayingContent = scanner.nextLine().trim();
-
-                System.out.println("작가: ");
-                String wiseSayingAuthor = scanner.nextLine().trim();
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
-
-                WiseSaying wiseSaying = new WiseSaying();
-                wiseSaying.id = lastId;
-                wiseSaying.content = wiseSayingContent;
-                wiseSaying.author = wiseSayingAuthor;
-
-                lastId++;
-            }
-        }
+        App app = new App();
+        app.run();
     }
 }
