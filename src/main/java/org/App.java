d6166e2b1c3e02435abfc9096491704a9ec26ee5
wooki0123 <wookimini22@gmail.com>
2026-04-09 12:33:09 +0900

    refactor: 기능 분리 리팩토링

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -23,8 +23,11 @@ public class App {
                 actionWrite();
             } else if (cmd.equals("목록")) {
                 actionList();
+            } else if (cmd.startsWith("삭제")) {
+                actionDelete(cmd);
             }
         }
+        scanner.close();
     }
     void exit() {
         System.out.println("프로그램 종료");
@@ -36,15 +39,19 @@ public class App {
         System.out.print("작가: ");
         String author = scanner.nextLine().trim();
 
-        System.out.printf("%d번 명언이 등록되었습니다.\n", id + 1);
+        WiseSaying wiseSaying = write(content, author);
 
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-
-
-        wiseSayingList.add(wiseSaying);
-
-        id++;
+        System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSaying.getId());
     }
+
+
+    WiseSaying write(String content, String author) {
+            WiseSaying wiseSaying = new WiseSaying(++id, content, author);
+            wiseSayingList.add(wiseSaying);
+
+            return wiseSaying;
+    }
+
     void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------------");
@@ -53,5 +60,35 @@ public class App {
             WiseSaying wiseSaying = wiseSayingList.get(i);
             System.out.printf("%d / %s / %s\n".formatted(wiseSaying.getId()+1, wiseSaying.getContent(), wiseSaying.getAuthor()));
         }
+    }
+    void actionDelete(String cmd) {
+        String[] cmdBits = cmd.split("=");
+        if (cmdBits.length < 2 || cmdBits[1].isEmpty()) {
+            System.out.println("ID 값 확인");
+            return;
+        }
+
+        int id = Integer.parseInt(cmdBits[1]);
+
+        delete(id);
+
+        System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
+    }
+
+    void delete(int id) {
+        WiseSaying wiseSaying = null;
+        for (int i = 0; i < wiseSayingList.size(); i++) {
+            if (wiseSayingList.get(i).getId() == id) {
+                wiseSaying = wiseSayingList.get(i);
+            }
+        }
+
+        if (wiseSaying == null) {
+            System.out.println("해당 아이디는 존재하지 않습니다.");
+            return;
+        }
+
+
+        wiseSayingList.remove(wiseSaying);
     }
 }
