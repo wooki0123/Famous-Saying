bf12889761bb9ed8edf14f43eb1d9cdda57772bb
wooki0123 <wookimini22@gmail.com>
2026-04-13 11:35:50 +0900

    feat: 시스템 컨트롤러 생성

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,5 +1,7 @@
 package org;
 
+import org.domain.system.SystemController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +12,8 @@ public class App {
     private final List<WiseSaying> wiseSayingList = new ArrayList<>();
 
     void run () {
+        SystemController systemController = new SystemController();
+
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -20,7 +24,7 @@ public class App {
 
             switch (rq.getActionName()) {
                 case "종료" -> {
-                    System.out.println("프로그램을 종료합니다.");
+                    systemController.actionExit();
                     return;
                 }
                 case "등록" -> actionWrite();
---
+++ src/main/java/org/domain/system/SystemController.java
@@ -0,0 +1,8 @@
+package org.domain.system;
+
+public class SystemController {
+
+    public void actionExit() {
+        System.out.println("프로그램을 종료합니다.");
+    }
+}
